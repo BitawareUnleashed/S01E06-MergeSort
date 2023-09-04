1fd60d6d72776579b44693adf14b25ed30634ce4
BitawareUnleashed <dario.benevento@gmail.com>
2023-09-04 20:34:26 +0200

    Updated array and restructured the position of methods.

--- S01E04-MergeSort/Program.cs
+++ S01E04-MergeSort/Program.cs
@@ -1,4 +1,14 @@
-﻿int[] arr = { 5, 2, 8, 4, 1, 10, 9, 6, 7, 3 };
+﻿int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+              11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+              21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+              31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+              41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+              51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+              61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+              71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+              81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+              91, 92, 93, 94, 95, 96, 97, 98, 99, 100 };
+
 
 Console.WriteLine("Array non ordinato:");
 PrintArray(arr, 0, arr.Length - 1);
@@ -11,6 +21,24 @@ PrintArray(arr, 0, arr.Length - 1);
 
 Console.ReadLine();
 
+
+static void MergeSort(int[] arr, int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+
+        Merge(arr, l, m, r);
+
+        Console.WriteLine($"Fusione: left = {l}, middle = {m}, right = {r}");
+        Console.Write("Array: ");
+        PrintArray(arr, l, r);
+        Console.WriteLine();
+    }
+}
 
 
 static void Merge(int[] arr, int left, int middle, int right)
@@ -60,23 +88,6 @@ static void Merge(int[] arr, int left, int middle, int right)
     }
 }
 
-static void MergeSort(int[] arr, int l, int r)
-{
-    if (l < r)
-    {
-        int m = l + (r - l) / 2;
-
-        MergeSort(arr, l, m);
-        MergeSort(arr, m + 1, r);
-
-        Merge(arr, l, m, r);
-
-        Console.WriteLine($"Fusione: left = {l}, middle = {m}, right = {r}");
-        Console.Write("Array: ");
-        PrintArray(arr, l, r);
-        Console.WriteLine();
-    }
-}
 
 static void PrintArray(int[] arr, int start, int end)
 {
